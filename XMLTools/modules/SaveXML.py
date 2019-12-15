ca024b3ed49f0fe0a4f47b295aa595d421a3e9a7
Emanuel Valero <evalero@seriola.cbm.usb.ve>
2019-12-14 19:14:31 -0500

    Creating and updating modules

--- XMLTools/modules/SaveXML.py
+++ XMLTools/modules/SaveXML.py
@@ -8,5 +8,31 @@ AUTHOR:
 LATEST UPDATE:
     2019-12-14
 """
-def exportXML(xmlfile, outputdir, outputname):
-    return
+
+import os, sys, uuid
+import xml.etree.ElementTree as ET
+
+def exportXML(xmlfile, outputname, outputdirs = ['..', 'outputs']):
+
+    outputdir = ''
+
+    for directory in outputdirs:
+        outputdir = os.path.join(outputdir, directory)
+
+    if not os.path.exists(outputdir):
+        os.makedirs( outputdir )
+
+    output = os.path.join( outputdir, outputname)
+
+    xmlfile.write( output, encoding = 'utf-8')
+
+    return xmlfile
+
+
+"""
+Implementation
+"""
+
+xmlfile = ET.parse('../inputs/DT00/dummy.xml')
+
+exportXML(xmlfile, 'filename.xml', outputdirs = ['..', 'outputs', 'anotherdir'])
